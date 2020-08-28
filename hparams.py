670a6cfe52567584deae25667874d1d064443046
Michal Pawlowicz <michal.pawlowicz@yahoo.com>
2020-08-28 09:50:55 +0000

    scripts

--- hparams.py
+++ hparams.py
@@ -3,7 +3,7 @@
 
 # Here are the input and output data paths (Note: you can override wav_path in preprocess.py)
 wav_path = '/path/to/wav_files/'
-data_path = 'data/'
+data_path = 'data_raw/'
 
 # model ids are separate - that way you can use a new tts with an old wavernn and vice versa
 # NB: expect undefined behaviour if models were trained on different DSP settings
@@ -35,7 +35,7 @@ peak_norm = False                   # Normalise to the peak of each wav file
 
 
 # Model Hparams
-voc_mode = 'MOL'                    # either 'RAW' (softmax on raw bits) or 'MOL' (sample from mixture of logistics)
+voc_mode = 'RAW'                    # either 'RAW' (softmax on raw bits) or 'MOL' (sample from mixture of logistics)
 voc_upsample_factors = (5, 5, 11)   # NB - this needs to correctly factorise hop_length
 voc_rnn_dims = 512
 voc_fc_dims = 512
