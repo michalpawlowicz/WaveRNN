e7d6bc5b6541f888a09c6281ba32ef27ca09bd8d
Michał Pawłowicz <michal.pawlowicz@yahoo.com>
2020-08-31 21:35:15 +0200

    resume training

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train_wavernn.py --lr 0.00001 --batch_size 32 --force_train --model_name model_raw_56_speakers_batch32_Adam_lr_1e-05 --adam --epoch 1102
+python train_wavernn.py --lr 0.00001 --batch_size 32 --force_train --model_name model_raw_56_speakers_batch32_Adam_lr_1e-05 --adam --epoch 1102 --checkpoint_name model_raw_56_speakers_batch32_Adam_lr_1e-04-epoch-1101-loss-2.2243492147988744
